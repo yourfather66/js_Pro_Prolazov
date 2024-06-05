a4e4bb44bd93fb730eea0b33400fa218a847419a
YourFather <igorprolazov@gmail.com>
2024-06-05 21:57:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let str = `Hello World`;
+let num = 123;
+let bool = true;
+let undef;
+let bInt = 1n;
+let nul = null;
+let fun = function () {};
+let arr = [1, 2, 3, 4];
+let obj = { a: 1, b: 2 };
+
+console.log(
+  typeof str,
+  typeof num,
+  typeof bool,
+  typeof undef,
+  typeof bInt,
+  typeof nul,
+  typeof fun,
+  typeof arr,
+  typeof obj
+);
